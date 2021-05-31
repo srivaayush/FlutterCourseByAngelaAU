b650afcecdf7af37486d283179b26c5b62ec8970
Ayush srivastava <ayush.sriva6@gmail.com>
2021-05-31 14:24:40 +0530

    Second  Comit!

--- CourseChallenges/magic_ball_challenge/lib/main.dart
+++ CourseChallenges/magic_ball_challenge/lib/main.dart
@@ -1,8 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlue[400],
+          appBar: AppBar(
+            backgroundColor: Colors.indigo,
+            title: Center(
+              child: Text(
+                "Ask Me Anything",
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+            ),
+          ),
+          body: HomeAMA(),
+        ),
       ),
     );
 
+class HomeAMA extends StatefulWidget {
+  @override
+  _HomeAMAState createState() => _HomeAMAState();
+}
+
+class _HomeAMAState extends State<HomeAMA> {
+  int numOnBall = 1;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: FlatButton(
+      onPressed: () {
+        predictFuture();
+      },
+      child: Center(
+        child: Container(
+          child: Image.asset('images/ball$numOnBall.png'),
+        ),
+      ),
+    ));
+  }
+
+  void predictFuture() {
+    setState(() {
+      numOnBall = Random().nextInt(5) + 1;
+    });
+  }
+}
