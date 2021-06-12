b0f712e4ad3df723313fc06df977421ef0ca6f87
Ayush srivastava <ayush.sriva6@gmail.com>
2021-06-13 00:08:57 +0530

    Clima App : Completed!

---
+++ CourseProjects/clima_app/lib/services/location.dart
@@ -0,0 +1,19 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude=0.0;
+  double longitude=0.0;
+
+  Future <void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+      print(position);
+      longitude = position.longitude;
+      latitude = position.latitude;
+    } 
+    catch (e) {
+      print(e);
+    }
+  }
+}
